b29aa523b688237a6be9368d7ca87cec12c3988d
Leo Ss <leo.vm.cba@gmail.com>
2025-04-28 22:22:27 -0300

    ✅ add: random phrases for footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,4 +10,17 @@ export class FooterComponent {
   currentYear: number = new Date().getFullYear();
   author: string = 'Leo Ss';
 
+  phrases = [
+    'Made with ❤',
+    'Powered by purrs and creativity 🐈',
+    'Developed with ❤️ and ☕',
+    'Coded under strict supervision of my cat 🐾 ',
+    'Just another line of code </>...',
+    'Programming with a cat on my lap 🐾',
+    'Dreamed, built, and shipped 🚀',
+    'Fueled by coffee and paw-sitive vibes 🐾'
+  ];
+
+  selectedPhrase: string = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+
 }
