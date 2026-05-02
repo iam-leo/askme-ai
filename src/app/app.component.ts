ce54ed09651cad493aecc39e77907c464371cc3f
Leo Ss <leo.vm.cba@gmail.com>
2026-05-02 19:12:29 -0300

    change: models with its respective endpoints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,55 @@ import { ScrollToggleButtonComponent } from './components/scroll-toggle-button/s
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,FormsModule, InputTextComponent, ChatComponent, FooterComponent, ScrollToggleButtonComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextComponent,
+    ChatComponent,
+    FooterComponent,
+    ScrollToggleButtonComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   messages: ChatMessage[] = [];
   isThinking = false;
-  selectedModel = 'meta-llama/llama-3.3-70b-instruct:free';
+  selectedModel = 'nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free';
   models = [
-    { name: 'Meta Llama 3.3 70B Instruct', value: 'meta-llama/llama-3.3-70b-instruct:free' },
-    { name: 'Google Gemini 2.0 Flash Exp', value: 'google/gemini-2.0-flash-exp:free' },
-    { name: 'DeepSeek R1 Qwen3 8B', value: 'deepseek/deepseek-r1-0528-qwen3-8b:free' },
-    { value: 'mistralai/devstral-small:free', name: 'Mistral DevStral Small' },
-    { value: 'opengvlab/internvl3-14b:free', name: 'OpenGVLab InternVL3 14B' },
-    { value: 'nvidia/llama-3.3-nemotron-super-49b-v1:free', name: 'NVIDIA Llama 3.3 Nemotron Super 49B V1' },
-    { name: 'Microsoft Mai Ds R1', value: 'microsoft/mai-ds-r1:free' }
+    {
+      name: 'NVIDIA Nemotron 3 Nano Omni',
+      value: 'nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free',
+    },
+    {
+      name: 'Google Gemma 4',
+      value: 'google/gemma-4-26b-a4b-it:free',
+    },
+    {
+      name: 'MiniMax M2.5',
+      value: 'minimax/minimax-m2.5:free',
+    },
+    {
+      name: 'Poolside Laguna XS.2',
+      value: 'poolside/laguna-xs.2:free',
+    },
+    {
+      name: 'OpenAI: gpt-oss-120b',
+      value: 'openai/gpt-oss-120b:free',
+    },
+    {
+      name: 'Baidu Qianfan-OCR-Fast',
+      value: 'baidu/qianfan-ocr-fast:free',
+    },
+    { name: 'LiquidAI LFM2.5-1.2B-Thinking',
+      value: 'liquid/lfm-2.5-1.2b-thinking:free'
+    },
   ];
 
-  constructor(private aiService: AiChatService, private changeDetector: ChangeDetectorRef) { }
+  constructor(
+    private aiService: AiChatService,
+    private changeDetector: ChangeDetectorRef,
+  ) {}
 
   ngOnInit() {
     // Cargar mensajes desde localStorage al iniciar
@@ -41,7 +71,7 @@ export class AppComponent implements OnInit {
       const rawMessages = JSON.parse(stored);
       this.messages = rawMessages.map((msg: any) => ({
         ...msg,
-        timestamp: new Date(msg.timestamp) // <- convertir a Date
+        timestamp: new Date(msg.timestamp), // <- convertir a Date
       }));
 
       this.messages = [...this.messages]; // Forzar la detección de cambios para que Angular actualice la vista
@@ -50,31 +80,40 @@ export class AppComponent implements OnInit {
 
   async handleNewMessage(message: string) {
     //this.messages.push({ from: 'user', content: message, timestamp: new Date() });
-    this.messages = [...this.messages, { from: 'user', content: message, timestamp: new Date() }];
+    this.messages = [
+      ...this.messages,
+      { from: 'user', content: message, timestamp: new Date() },
+    ];
 
     // Guardar mensajes del user en localStorage
     this.saveMessagesToLocalStorage();
 
     // Respuesta de AI
-    const aiMessage: ChatMessage = { from: 'ai', content: '', timestamp: new Date() };
+    const aiMessage: ChatMessage = {
+      from: 'ai',
+      content: '',
+      timestamp: new Date(),
+    };
     //this.messages.push(aiMessage);
     this.messages = [...this.messages, aiMessage];
-
 
     // Mostrar Spinner
     this.isThinking = true;
 
     // Renderizar a medida que llegan los chunks
     try {
-      for await (const chunk of this.aiService.getStreamedResponse(message, this.selectedModel)) {
+      for await (const chunk of this.aiService.getStreamedResponse(
+        message,
+        this.selectedModel,
+      )) {
         for (const char of chunk) {
           aiMessage.content += char;
-              // Reemplazar el último mensaje AI con una nueva copia (para cambiar la referencia)
+          // Reemplazar el último mensaje AI con una nueva copia (para cambiar la referencia)
           this.messages[this.messages.length - 1] = { ...aiMessage };
 
           // Forzar la actualización al cambiar el array (nueva referencia)
           this.messages = [...this.messages];
-          await new Promise(resolve => setTimeout(resolve, 30));
+          await new Promise((resolve) => setTimeout(resolve, 30));
         }
       }
       aiMessage.timestamp = new Date(); // Actualizar timestamp después de recibir la respuesta
@@ -85,18 +124,21 @@ export class AppComponent implements OnInit {
       // Guardar mensajes de AI en localStorage
       this.saveMessagesToLocalStorage();
     } catch (error: any) {
-        console.error('App Component Error:', error);
+      console.error('App Component Error:', error);
 
-        if (error.message === 'RATE_LIMIT_EXCEEDED') {
-          aiMessage.content = '🚫 Has alcanzado tu límite de uso diario gratuito. Vuelve mañana o compra créditos.';
-        } else if (error.message === 'NO_RESPONSE') {
-          aiMessage.content = '⚠️ El modelo no pudo generar una respuesta. Intenta de nuevo más tarde.';
-        } else {
-          aiMessage.content = '⚠️ Ocurrió un error inesperado. Intenta nuevamente.';
-        }
+      if (error.message === 'RATE_LIMIT_EXCEEDED') {
+        aiMessage.content =
+          '🚫 Has alcanzado tu límite de uso diario gratuito. Vuelve mañana o compra créditos.';
+      } else if (error.message === 'NO_RESPONSE') {
+        aiMessage.content =
+          '⚠️ El modelo no pudo generar una respuesta. Intenta de nuevo más tarde.';
+      } else {
+        aiMessage.content =
+          '⚠️ Ocurrió un error inesperado. Intenta nuevamente.';
+      }
     } finally {
-        this.isThinking = false; // Ocultar spinner
-        //this.changeDetector.detectChanges();
+      this.isThinking = false; // Ocultar spinner
+      //this.changeDetector.detectChanges();
     }
   }
 
@@ -108,5 +150,4 @@ export class AppComponent implements OnInit {
     this.messages = [];
     localStorage.removeItem('chatMessages');
   }
-
 }
